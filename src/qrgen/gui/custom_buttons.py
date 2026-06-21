178bc0478c7f6620325371de1880dff48d185563
Saber_0324 <saber0324@proton.me>
2026-06-21 19:10:15 -0400

    Added assets dir, drop_target.py, UploadButton, color_picker.py and constants.py

--- src/qrgen/gui/custom_buttons.py
+++ src/qrgen/gui/custom_buttons.py
@@ -1,5 +1,7 @@
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtGui
 from PySide6.QtGui import QColor
+from qrgen.gui.drop_target import ImageDropTarget
+from qrgen.gui.constants import ASSETS_DIR
 
 
 class ColorButton(QtWidgets.QPushButton):
@@ -15,3 +17,30 @@ class ColorButton(QtWidgets.QPushButton):
         if selected_color.isValid():
             self.color = selected_color
         self.setStyleSheet(f"background-color: {self.color.name()}")
+
+
+class UploadButton(QtWidgets.QGroupBox):
+    def __init__(self, text: str):
+        super().__init__(text)
+        upload_button_icon = QtGui.QPixmap(str(ASSETS_DIR / "upload.svg"))
+
+        layout = QtWidgets.QHBoxLayout(self)
+        self.upload_label = QtWidgets.QLabel("No image found")
+        upload_button = QtWidgets.QPushButton()
+        upload_button.setIcon(upload_button_icon)
+        upload_button.clicked.connect(self._on_upload_button_clicked)
+
+        layout.addWidget(self.upload_label)
+        layout.addWidget(upload_button)
+
+        self.upload_dialog = QtWidgets.QDialog()
+        self.upload_dialog.setWindowTitle("qrgen")
+        upload_layout = QtWidgets.QHBoxLayout(self.upload_dialog)
+        self.upload_dropable = ImageDropTarget()
+        upload_layout.addWidget(self.upload_dropable)
+
+    def _on_upload_button_clicked(self):
+        result = self.upload_dialog.exec()
+        if result == QtWidgets.QDialog.DialogCode.Rejected:
+            return
+        self.upload_label.setText(f"Icon: {self.upload_dropable._dropped_file}")
--- src/qrgen/gui/main_widget.py
+++ src/qrgen/gui/main_widget.py
@@ -1,12 +1,22 @@
 from PySide6 import QtCore, QtGui, QtWidgets
-from qrgen.qr_code import qr_generator
+from PySide6.QtCore import Qt
+from qrgen.gui.custom_buttons import UploadButton
 from qrgen.qr_code.qr_generator import generated_path
 from qrgen.gui.module_card import ModuleCard
+from qrgen.gui.color_picker import ColorPickWdg
 
 
 class MainWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+
+        last_generated_pixmap = QtGui.QPixmap(str(generated_path / "qrcode"))
+        scaled_pixmap = last_generated_pixmap.scaled(
+            200,
+            200,
+            Qt.AspectRatioMode.KeepAspectRatio,
+            Qt.TransformationMode.SmoothTransformation,
+        )
 
         self.setWindowTitle("qrgen")
         top_layout = QtWidgets.QHBoxLayout(self)
@@ -22,20 +32,58 @@ class MainWidget(QtWidgets.QWidget):
         preview = QtWidgets.QFrame()
         preview.setFixedWidth(250)
 
+        preview_layout = QtWidgets.QVBoxLayout()
+        preview_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+        preview.setLayout(preview_layout)
+
+        preview_label = QtWidgets.QLabel()
+        preview_label.setPixmap(scaled_pixmap)
+
+        upload_button = UploadButton("Upload an image")
+
+        preview_layout.addWidget(preview_label)
+        preview_layout.addWidget(upload_button)
+
         top_layout.addWidget(scroll)
         top_layout.addWidget(preview)
-
-        last_generated_pixmap = QtGui.QPixmap(str(generated_path / "qrcode"))
-        display_label = QtWidgets.QLabel()
-        display_label.setSizePolicy(
-            QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.Minimum
-        )
-        display_label.setPixmap(last_generated_pixmap)
 
         content_card = ModuleCard("CONTENT")
         content_card.add_widget(QtWidgets.QLabel("Your website URL"))
         content_card.add_widget(QtWidgets.QLineEdit())
         container_layout.addWidget(content_card)
+
+        design_card = ModuleCard("DESIGN")
+        design_card.add_widget(ColorPickWdg("Background color", "#FFF"))
+        design_card.add_widget(ColorPickWdg("QR color", "#000"))
+        container_layout.addWidget(design_card)
+
+
+class ImageDropTarget(QtWidgets.QLabel):
+    def __init__(self):
+        super().__init__("Drop Image Here")
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setStyleSheet("border: 2px dashed #aaaaaa")
+        self.setFixedSize(150, 50)
+        self.setAcceptDrops(True)
+
+        self._dropped_file = None
+
+    def dragEnterEvent(self, event):
+        mime = event.mimeData()
+        if not mime.hasUrls():
+            return
+        elif len(mime.urls()) != 1:
+            return
+
+        url = mime.urls()[0]
+        local_path = url.toLocalFile()
+        if local_path.endswith(".png"):
+            event.accept()
+            self._dropped_file = local_path
+
+    def dropEvent(self, event):
+        pix = QtGui.QPixmap(self._dropped_file).scaled(100, 100)
+        self.setPixmap(pix)
 
 
 def run_gui():
