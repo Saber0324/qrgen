733d874453ffbb63843e7175ea99416838f65c9a
Saber_0324 <saber0324@proton.me>
2026-06-21 19:11:03 -0400

    ruff format

--- src/qrgen/gui/main_widget.py
+++ src/qrgen/gui/main_widget.py
@@ -58,34 +58,6 @@ class MainWidget(QtWidgets.QWidget):
         container_layout.addWidget(design_card)
 
 
-class ImageDropTarget(QtWidgets.QLabel):
-    def __init__(self):
-        super().__init__("Drop Image Here")
-        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.setStyleSheet("border: 2px dashed #aaaaaa")
-        self.setFixedSize(150, 50)
-        self.setAcceptDrops(True)
-
-        self._dropped_file = None
-
-    def dragEnterEvent(self, event):
-        mime = event.mimeData()
-        if not mime.hasUrls():
-            return
-        elif len(mime.urls()) != 1:
-            return
-
-        url = mime.urls()[0]
-        local_path = url.toLocalFile()
-        if local_path.endswith(".png"):
-            event.accept()
-            self._dropped_file = local_path
-
-    def dropEvent(self, event):
-        pix = QtGui.QPixmap(self._dropped_file).scaled(100, 100)
-        self.setPixmap(pix)
-
-
 def run_gui():
     app = QtWidgets.QApplication()
     widget = MainWidget()
