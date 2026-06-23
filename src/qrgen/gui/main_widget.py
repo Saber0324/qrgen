a8f59a1b5e99b050e5a60b13a7ede4aedc965eed
Saber_0324 <saber0324@proton.me>
2026-06-23 00:58:54 -0400

    Added tabs, move upload button to tabs and created gradient config

--- src/qrgen/gui/main_widget.py
+++ src/qrgen/gui/main_widget.py
@@ -1,5 +1,6 @@
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtCore import Qt
+from qrgen.gui.constants import ASSETS_DIR
 from qrgen.gui.custom_buttons import InsertUrl, UploadButton
 from qrgen.qr_code.qr_generator import generated_path
 from qrgen.gui.custom_widgets import ModuleCard, ColorPickWdg
@@ -18,6 +19,7 @@ class MainWidget(QtWidgets.QWidget):
         )
 
         self.setWindowTitle("qrgen")
+        self.setMinimumSize(700, 400)
         top_layout = QtWidgets.QHBoxLayout(self)
         scroll = QtWidgets.QScrollArea()
         scroll.setWidgetResizable(True)
@@ -48,28 +50,114 @@ class MainWidget(QtWidgets.QWidget):
         preview_label.setPixmap(scaled_pixmap)
         preview_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        upload_button = UploadButton("Upload an image")
-
         preview_layout.addWidget(preview_label)
-        preview_layout.addWidget(upload_button)
 
         top_layout.addWidget(scroll)
         top_layout.addWidget(preview)
 
-        self.qr_color_button = ColorPickWdg("Background color", "#FFF")
-        self.bg_color_button = ColorPickWdg("QR color", "#000")
+        tabs_widget = QtWidgets.QTabWidget()
+        tabs_widget.setTabShape(QtWidgets.QTabWidget.TabShape.Rounded)
+        tabs_widget.setStyleSheet("""
+                        QTabWidget::pane {
+                            background: transparent;
+                            border: 3px;
+                        }
+                        QTabBar {
+                            background: transparent;
+                        }
+                        QTabBar::tab {
+                            padding: 3px 10px;
+                            border-radius: 10px;
+                            margin: 2px;
+                        }
+                        QTabBar::tab:selected {
+                            background: lightgrey;
+                            color: black;
+                        }
+        """)
+
+        self.qr_color_button = ColorPickWdg("QR color", "#000")
+        self.bg_color_button = ColorPickWdg("Background color", "#FFF")
+
+        self.grad_options_combo = QtWidgets.QComboBox()
+        self.grad_options_combo.addItems(["None", "Vertical", "Circular", "Horizontal"])
+        self.grad_options_combo.setBaseSize(70, 30)
+        self.grad_options_combo.currentTextChanged.connect(self._on_grad_option_change)
+        self.grad_options_combo.setStyleSheet("""
+                QComboBox {
+                    border: 1px solid #606090;
+                    border-radius: 6px;
+                    padding: 0px 8px;
+                }
+                QComboBox::drop-down {
+                    width: 0px;
+                }
+                QComboBox QAbstractItemView {
+                    border-radius: 6px;
+                    border: 1px solid #606090;
+
+                } """)
+        self.grad_options_combo.setSizeAdjustPolicy(
+            QtWidgets.QComboBox.SizeAdjustPolicy.AdjustToContents
+        )
+
+        self.grad_start_button = ColorPickWdg("Start color", "#000")
+        self.grad_end_button = ColorPickWdg("End color", "#000")
+
+        color_widget = QtWidgets.QWidget()
+
+        self.color_main_layout = QtWidgets.QVBoxLayout(color_widget)
+
+        color_button_layout = QtWidgets.QHBoxLayout()
+
+        self.gradient_widget = QtWidgets.QWidget()
+        self.gradient_buttons_layout = QtWidgets.QHBoxLayout(self.gradient_widget)
+
+        gradient_config_widget = QtWidgets.QWidget()
+        gradient_config_layout = QtWidgets.QHBoxLayout(gradient_config_widget)
+
+        gradient_config_label = QtWidgets.QLabel("Select gradient style:")
+
+        gradient_config_layout.addWidget(gradient_config_label)
+        gradient_config_layout.addWidget(self.grad_options_combo)
+        gradient_config_layout.addStretch()
+
+        self.gradient_buttons_layout.addWidget(self.grad_start_button)
+        self.gradient_buttons_layout.addWidget(self.grad_end_button)
+        self.gradient_buttons_layout.setContentsMargins(0, 0, 0, 0)
+
+        self.color_main_layout.addWidget(gradient_config_widget)
+        self.color_main_layout.addLayout(color_button_layout)
+
+        color_button_layout.addWidget(self.qr_color_button)
+        color_button_layout.addWidget(self.bg_color_button)
+
+        self.color_main_layout.addWidget(self.gradient_widget)
+        self.gradient_widget.setVisible(False)
+
+        tabs_widget.addTab(color_widget, "Color")
+
+        self.upload_button = UploadButton("Upload an image")
+
+        tabs_widget.addTab(self.upload_button, "Logo")
+
+        tabs_widget.addTab(QtWidgets.QWidget(), "Shape")
+
         content_card = ModuleCard("CONTENT")
         content_card.add_widget(InsertUrl("Insert url"))
         container_layout.addWidget(content_card)
+
         container_layout.setSpacing(10)
 
         design_card = ModuleCard("DESIGN")
-        design_card.add_widget(self.qr_color_button)
-        design_card.add_widget(self.bg_color_button)
+        design_card.add_widget(tabs_widget)
         container_layout.addWidget(design_card)
 
     def _on_bg_color_selected(self):
         print(self.bg_color_button.color)
+
+    def _on_grad_option_change(self, text):
+        self.gradient_widget.setVisible(text != "None")
 
 
 def run_gui():
