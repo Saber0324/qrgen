816227e0bec9433394d31614fc8c2f7a28269b47
Saber_0324 <saber0324@proton.me>
2026-06-23 00:43:37 -0400

    Turn the file_path into a Path object if it exits. Change manual svg recolor for helpers.recolor_svg

--- src/qrgen/gui/custom_buttons.py
+++ src/qrgen/gui/custom_buttons.py
@@ -1,37 +1,15 @@
+from pathlib import Path
 from PySide6 import QtWidgets, QtGui, QtCore, QtSvg
 from PySide6.QtCore import QSize
-from PySide6.QtGui import QColor
-from qrgen.gui.custom_widgets import ImageDropTarget
 from qrgen.gui.constants import ASSETS_DIR
-
-
-class ColorButton(QtWidgets.QPushButton):
-    def __init__(self, color: QColor = QColor("#000000")):
-        super().__init__()
-        self.color = color
-
-        self.setStyleSheet(f"background-color: {self.color.name()};")
-        self.clicked.connect(self.open_color_dialog)
-
-    def open_color_dialog(self):
-        selected_color = QtWidgets.QColorDialog().getColor()
-        if selected_color.isValid():
-            self.color = selected_color
-        self.setStyleSheet(f"background-color: {self.color.name()}")
+from qrgen.gui.helpers import recolor_svg
 
 
 class UploadButton(QtWidgets.QGroupBox):
     def __init__(self, text: str):
         super().__init__(text)
         upload_icon_path = ASSETS_DIR / "upload.svg"
-        upload_icon_text = upload_icon_path.read_text()
-        upload_icon_text = upload_icon_text.replace('fill="#000000"', 'fill="#606090"')
-        renderer = QtSvg.QSvgRenderer(QtCore.QByteArray(upload_icon_text.encode()))
-        pixmap = QtGui.QPixmap(32, 32)
-        pixmap.fill(QtCore.Qt.GlobalColor.transparent)
-        painter = QtGui.QPainter(pixmap)
-        renderer.render(painter)
-        painter.end()
+
         self.setStyleSheet("""
             QGroupBox {
                 border: 1px solid #606090;
@@ -45,11 +23,12 @@ class UploadButton(QtWidgets.QGroupBox):
                 padding: 0 3px;
             }
         """)
+        self.setMaximumHeight(80)
 
         layout = QtWidgets.QHBoxLayout(self)
         self.upload_label = QtWidgets.QLabel("No image selected")
         upload_button = QtWidgets.QPushButton()
-        upload_button.setIcon(QtGui.QIcon(pixmap))
+        upload_button.setIcon(QtGui.QIcon(recolor_svg(upload_icon_path, "#606090")))
         upload_button.setFixedSize(QSize(30, 30))
         upload_button.setFlat(True)
         upload_button.clicked.connect(self._on_upload_button_clicked)
@@ -59,16 +38,17 @@ class UploadButton(QtWidgets.QGroupBox):
 
         self.upload_dialog = QtWidgets.QDialog()
         self.upload_dialog.setWindowTitle("qrgen")
-        upload_layout = QtWidgets.QHBoxLayout(self.upload_dialog)
-        self.upload_dropable = ImageDropTarget()
-        upload_layout.addWidget(self.upload_dropable)
 
     def _on_upload_button_clicked(self, dialog: QtWidgets.QDialog):
         file_path, _ = QtWidgets.QFileDialog.getOpenFileName(
-            self, "Select Image", "", "Images (*.png *.jpg *.jpeg *.svg)"
+            self,
+            "Select Image",
+            "",
+            "Images (*.png *.jpg *.jpeg *.svg)",
         )
         if file_path:
-            self.upload_label.setText(f"{file_path.split('/')[-1]}")
+            file_path = Path(file_path)
+            self.upload_label.setText(f"{file_path.name}")
             dialog.accept()
 
 
