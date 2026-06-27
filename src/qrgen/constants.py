0ff6d6756ba09757a9c9cdb4066f87c8c14522d0
Saber_0324 <saber0324@proton.me>
2026-06-27 00:36:19 -0400

    Changed GENERATED_DIR to use Documents/qrgen

--- src/qrgen/constants.py
+++ src/qrgen/constants.py
@@ -3,5 +3,5 @@ from pathlib import Path
 ASSETS_DIR = Path(__file__).parent / "gui" / "assets"
 ASSETS_DIR.mkdir(exist_ok=True)
 
-GENERATED_DIR = Path(__file__).parent / "generated"
-GENERATED_DIR.mkdir(exist_ok=True)
+GENERATED_DIR = Path.home() / "Documents" / "qrgen"
+GENERATED_DIR.mkdir(parents=True, exist_ok=True)
